0c666281c575eb6f30a60a3eab8e4c064cf5b1eb
StevenCellist <47155822+StevenCellist@users.noreply.github.com>
2021-03-19 18:02:11 +0100

    Corrected typos, removed variable minimum

--- Sequences-negative.cpp
+++ Sequences-negative.cpp
@@ -1,6 +1,6 @@
 // Made by Steven Boonstoppel, with crucial speed improvements thanks to Vladimir Feinstein, algorithm by Levi van de Pol
 // First version: 18-11-2020; estimated time to length 48: 250 years*
-// Current version: 15-03-2021; completed time to length 48: 0.5 second*
+// Current version: 19-03-2021; completed time to length 48: 98 milliseconds*
 // * reference CPU: AMD Ryzen 7 3800X, 16 threads @ ~4.2 GHz boost, Microsoft VS Studio 2019 Compiler
 
 #include <iostream>
@@ -44,13 +44,13 @@ std::mutex m_tails, m_kp;
 int g_k1 = 2, g_p1 = 1;
 
 std::unordered_map<int, int> expected_tails = {
-    {2, 2},     {4, 4},     {6, 8},     {8, 58},    {9, 59},     {10, 60},    {11, 112},   {14, 118},   {19, 119},  {22, 120}, 
-    {48, 131},  {68, 132},  {73, 133},  {77, 173},  {85, 179},   {115, 215},  {116, 228},  {118, 229},  {128, 332}, {132, 340},
-    {133, 342}, {149, 343}, {154, 356}, {176, 406}, {197, 1668}, {198, 1669}, {200, 1670}, {208, 1706}, {217, 1836}
+    {2, 2},     {4, 4},     {6, 8},     {8, 58},    {9, 59},     {10, 60},    {11, 112},   {14, 118},   {19, 119},   {22, 120},
+    {48, 131},  {68, 132},  {73, 133},  {77, 173},  {85, 179},   {115, 215},  {116, 228},  {118, 229},  {128, 332},  {132, 340},
+    {133, 342}, {149, 343}, {154, 356}, {176, 406}, {197, 1668}, {199, 1669}, {200, 1670}, {208, 1708}, {217, 1836}, {290, 3382}
 };
 
 bool diff(const int16_t* p1, const int16_t* p2, int count) {
-    int count64 = count / 4; // four 16-bit ints in uint64_t
+    int count64 = count / 4;    // four 16-bit ints in uint64_t
     if (count64) {
         uint64_t* p1_64 = (uint64_t*)p1;
         uint64_t* p2_64 = (uint64_t*)p2;
@@ -60,12 +60,12 @@ bool diff(const int16_t* p1, const int16_t* p2, int count) {
         }
     }
 
-    int count4 = count % 4;
-    if (count4) {
-        int offset = (count / 4) * 4;
+    int count16 = count % 4;    // left-overs
+    if (count16) {
+        int offset = count - count16;
         p1 += offset;
         p2 += offset;
-        while (count4--) {
+        while (count16--) {
             if (*p1++ != *p2++)
                 return true;
         }
@@ -73,9 +73,9 @@ bool diff(const int16_t* p1, const int16_t* p2, int count) {
     return false;
 }
 
-int krul(const val_vector& s, int& period, int l, int minimum) {
-    int curl = minimum - 1;                                 // base value for curl
-    int limit = l / minimum;                                // limit up to which to check for repetition
+int krul(const val_vector& s, int& period, int l) {
+    int curl = 1;                                           // base value for curl
+    int limit = l / 2;                                      // limit up to which to check for repetition
     for (int i = 1; i <= limit; ++i) {                      // check for repetition up to the limit
         const int16_t* p1 = &s[l - i];                      // start of the last pattern
         const int16_t* p2 = p1 - i;                         // start of the previous pattern
@@ -98,7 +98,7 @@ void up() {
         if (periods.empty())                                        // stop if tail is empty
             break;
         if ((candidatecurl * candidateperiod) <= seq.size())        // if this pair is within sequence size, we can break and try that instead
-            break; 
+            break;
         if (candidatecurl > seq.back()) {                           // if curl > last_curl, the sequence will have to change if it wants to become better
             int16_t k = (int16_t)periods.size() - 1;                // we will have to change the for-last curl in order to change the tail, 
             auto index = change_indices.find(k);                    // so let's check whether it has been done yet
@@ -140,7 +140,7 @@ void append() {
 
     int period = 0;
     while (true) {                                          // build the tail for this generator
-        int curl = krul(seq, period, (int)seq.size(), 2);
+        int curl = krul(seq, period, (int)seq.size());
         if (curl == 1)
             break;
         seq.push_back((int16_t)curl);
@@ -173,7 +173,7 @@ bool test_1() {
     l = (int)seq.size() - 1;                                // reset pattern values
     lcp = l - candidateperiod;
 
-    int jmax = length - 1;                                  // get the index of the last negative value in seq_new to limit the for loop below
+    int jmax = length - 1;                                  // get the index of the last negative value in seq_new to limit the for-loop below
     for (; jmax > 0; --jmax) {
         if (seq_new[jmax] < 0)
             break;
@@ -201,18 +201,18 @@ bool test_1() {
     return true;
 }
 
-// this function checks whether the tail will contain anything meaningful
+// this function checks whether the proposed change invalidates the generator (regarding curl or period)
 bool test_2() {
     int l = (int)seq_new.size();
     seq_new.push_back((int16_t)candidatecurl);                              // add the candidates because we passed test_1
     int period = 0;
 
     for (int i = 0; i < l - length; ++i) {                                  // check within tail for a valid curl or period
-        int curl = krul(seq_new, period, length + i, seq_new[length + i]);  // calculate curl and period up to this part of the sequence
+        int curl = krul(seq_new, period, length + i);                       // calculate curl and period up to this part of the sequence
         if (curl != seq_new[length + i] or period != periods[i])            // if the curl or its period are not related, the tail will not improve
             return false;
     }
-    int curl = krul(seq_new, period, l, seq_new[l]);
+    int curl = krul(seq_new, period, l);
     return (curl == seq_new[l] and period == candidateperiod);
 }
 
@@ -244,7 +244,7 @@ void backtracking() {
         }
         for (int i = 0; i < length; ++i)                // initiate sequence
             seq[i] = (int16_t)(-length + i);
-        
+
         backtracking_step();                            // perform backtracking for this combination (g_k1, g_p1)
         while (periods.size())
             backtracking_step();
@@ -266,10 +266,10 @@ void multi_threader() {
     std::vector<std::thread> thread_vector;
     if (length <= 80)
         thread_vector.emplace_back(std::thread(backtracking));
-    else 
+    else
         for (int i = 0; i < thread_count; ++i)
             thread_vector.emplace_back(std::thread(backtracking));
-    for (auto& th : thread_vector) 
+    for (auto& th : thread_vector)
         th.join();
 }
 
